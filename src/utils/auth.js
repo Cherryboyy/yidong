16e34247b061bcad66f42c4d194416f71dcf4f7b
cmy <1732728869@qq.com>
2020-01-11 20:34:12 +0800

    设置用户的token值，用vuex管理token值

---
+++ src/utils/auth.js
@@ -0,0 +1,17 @@
+//处理用户的token
+const USE_TOKEN = 'heima-toutiao-m'
+
+//设置用户的信息
+export function setUser(user) {
+  localStorage.setItem(USE_TOKEN,JSON.stringify(user))
+}
+
+//读取用户的信息
+export function getUser() {
+  return JSON.parse(localStorage.getItem(USE_TOKEN) || '{}')
+}
+
+//删除用户信息
+export function delUser() {
+  localStorage.removeItem(USE_TOKEN)
+}
