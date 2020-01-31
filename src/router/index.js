dd0221819d7c6f838c27ef41144084ab136beb3e
cmy <1732728869@qq.com>
2020-01-31 19:59:56 +0800

    登录完结

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+// 导入vuex管理的toenk
+import store from '@/store'
 // import Home from '../views/home/index.vue'
-const Layout = () => import('../views/layout')// 布局的组件
-const Home = () => import('../views/home/index')// 主页组件
-const Question = () => import('../views/home/question')// 问答组件
-const Video = () => import('../views/home/video')// 视频组件
-const User = () => import('../views/home/user')// 个人中心
-const Profile = () => import('../views/home/user/profile')//编辑资料
-const Chat = () => import('../views/home/user/chat')// 小智同学
-const Login = () => import('../views/login')//登录组件
-const Article = () => import('../views/article')//文章详情
-const Search = () => import('../views/search')//搜索中心
-const SearchResult = () => import('../views/search/result')//搜索结果
+const Layout = () => import('../views/layout') // 布局的组件
+const Home = () => import('../views/home/index') // 主页组件
+const Question = () => import('../views/home/question') // 问答组件
+const Video = () => import('../views/home/video') // 视频组件
+const User = () => import('../views/home/user') // 个人中心
+const Profile = () => import('../views/home/user/profile') //编辑资料
+const Chat = () => import('../views/home/user/chat') // 小智同学
+const Login = () => import('../views/login') //登录组件
+const Article = () => import('../views/article') //文章详情
+const Search = () => import('../views/search') //搜索中心
+const SearchResult = () => import('../views/search/result') //搜索结果
 
 Vue.use(VueRouter)
 
 
-const routes = [
-  {
+const routes = [{
+  path: '/',
+  component: Layout, //一级路由
+  children: [{ //二级路由
     path: '/',
-    component: Layout,//一级路由
-    children: [{//二级路由
-      path: '/',
-      component: Home//首页
-    }, {
-      path: '/question',
-      component: Question//问答组件
-    }, {
-      path: '/video',
-      component: Video//视频组件
-    }, {
-      path: '/user',
-      component: User//个人中心
-    }]
+    component: Home //首页
   }, {
-    path: '/user/profile',
-    component: Profile//编辑资料
+    path: '/question',
+    component: Question //问答组件
   }, {
-    path: '/user/chat',
-    component: Chat//小智同学
+    path: '/video',
+    component: Video //视频组件
   }, {
-    path: '/login',
-    component: Login//登录
-  }, {
-    path: '/article',
-    component: Article//文章详情
-  }, {
-    path: '/search',
-    component: Search//搜索
-  }, {
-    path: '/search/result',
-    component: SearchResult//搜索结果
-  }
-]
+    path: '/user',
+    component: User //个人中心
+  }]
+}, {
+  path: '/user/profile',
+  component: Profile //编辑资料
+}, {
+  path: '/user/chat',
+  component: Chat //小智同学
+}, {
+  path: '/login',
+  component: Login //登录
+}, {
+  path: '/article',
+  component: Article //文章详情
+}, {
+  path: '/search',
+  component: Search //搜索
+}, {
+  path: '/search/result',
+  component: SearchResult //搜索结果
+}]
 
 const router = new VueRouter({
   routes
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='container'>
+  <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
     <van-cell-group>
       <van-field
@@ -19,71 +19,70 @@
         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
       </van-field>
     </van-cell-group>
-    <div class='btn-box'>
+    <div class="btn-box">
       <van-button type="info" size="small" @click="login" round block>登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
-  import { login } from '../../api/user'
-  import { mapMutations } from 'vuex'
+import { login } from '../../api/user'
+import { mapMutations } from 'vuex'
 
-  export default {
-    data () {
-      return {
-        loginForm: {
-          mobile: '13911111111',
-          code: '246810'
-        },
-        //专门的提示信息
-        errMsg: {
-          mobile: '',
-          code: ''
-        }
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      // 专门的提示信息
+      errMsg: {
+        mobile: '',
+        code: ''
       }
+    }
+  },
+  methods: {
+    checkMobile () {
+      //  判断 为空 判断格式
+      if (!this.loginForm.mobile) {
+        this.errMsg.mobile = '手机号不能为空'
+        return false
+      }
+      // 判断格式
+      if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
+        this.errMsg.mobile = '手机格式不正确'
+        return false
+      }
+      this.errMsg.mobile = '' // 清空信息
+      return true
     },
-    methods: {
-      checkMobile () {
-        //  判断 为空 判断格式
-        if (!this.loginForm.mobile) {
-          this.errMsg.mobile = '手机号不能为空'
-          return false
-        }
-        // 判断格式
-        if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
-          this.errMsg.mobile = '手机格式不正确'
-          return false
-        }
-        this.errMsg.mobile = '' // 清空信息
-        return true
-      },
-      validCode () {
-        if (!this.loginForm.code) {
-          this.errMsg.code = '验证码不能为空'
-          return false
-        }
-        if (!/^\d{6}$/.test(this.loginForm.code)) {
-          this.errMsg.code = '验证码必须为6位数字'
-          return false
-        }
-        this.errMsg.code = '' // 清空信息
-        return true
-      },
-      ...mapMutations(['updateUser']),
-      async login () {
-
-        if (this.checkMobile() && this.validCode()) {
-          const data = await login(this.loginForm)
-          console.log(data)
-        }
+    validCode () {
+      if (!this.loginForm.code) {
+        this.errMsg.code = '验证码不能为空'
+        return false
+      }
+      if (!/^\d{6}$/.test(this.loginForm.code)) {
+        this.errMsg.code = '验证码必须为6位数字'
+        return false
+      }
+      this.errMsg.code = '' // 清空信息
+      return true
+    },
+    ...mapMutations(['updateUser']),
+    async login () {
+      if (this.checkMobile() && this.validCode()) {
+        const data = await login(this.loginForm)
+        console.log(data)
       }
     }
   }
+}
 </script>
 
 <style scoped>
-  .btn-box {
-    padding: 20px;
-  }
+.btn-box {
+  padding: 20px;
+}
 </style>
