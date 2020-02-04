d5944d7b86c01d8b61201d47aac8c5bdd1c0f11b
cmy <1732728869@qq.com>
2020-02-04 21:26:48 +0800

    搜索结果完成

--- src/views/search/result/index.vue
+++ src/views/search/result/index.vue
@@ -1,17 +1,101 @@
-<template>
-  <div class='container'>
-    <!-- 搜索组件一级路由   返回上一个页面-->
-    <van-nav-bar left-arrow title='搜索结果' @click-left="$router.back()"></van-nav-bar>
-    <!-- 导航 -->
+ <template>
+  <div class="container">
+    <van-nav-bar fixed title="搜索结果" left-arrow @click-left="$router.back()" />
+    <van-list v-model="upLoading" :finished="finished" @load="onLoad">
+      <van-cell-group>
+        <!-- art_id有可能是大数字 超过一定长度会转成 BigNumber  要toString一下 -->
+        <van-cell v-for="item in articles" :key="item.art_id.toString()">
+          <div class="article_item">
+            <h3 class="van-ellipsis">{{ item.title }}</h3>
+            <div class="img_box" v-if="item.cover.type === 3">
+              <!-- 支持图片懒加载   当图片显示在当前可视区域的时候才去请求该图片的地址 -->
+              <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[0]" />
+              <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[1]" />
+              <van-image lazy-load class="w33" fit="cover" :src="item.cover.images[2]" />
+            </div>
+            <div class="img_box" v-if="item.cover.type === 1">
+              <van-image lazy-load class="w100" fit="cover" :src="item.cover.images[0]" />
+            </div>
+            <div class="info_box">
+              <span>{{ item.aut_name }}</span>
+              <span>{{item.comm_count}}评论</span>
+              <span>{{ item.pubdate | relTime }}</span>
+            </div>
+          </div>
+        </van-cell>
+      </van-cell-group>
+    </van-list>
   </div>
 </template>
 
 <script>
-  export default {
-    name: ''
+import { searchArticle } from "../../../api/article";
+export default {
+  name: "",
+  data() {
+    return {
+      upLoading: false,
+      finished: false,
+      articles: [],
+      page: {
+        page: 1,
+        per_page: 10
+      }
+    };
+  },
+  methods: {
+    async onLoad() {
+      let { q } = this.$route.query; // 从地址栏解析查询参数
+      let data = await searchArticle({ ...this.page, q });
+      console.log(data);
+      
+      // 上拉加载的业务  =>  追加到队尾
+      this.articles.push(...data.results);
+      // 手动关闭加载状态
+      this.upLoading = false;
+      // 根据当前返回的 数组的长度 如果当前的长度为0 表示下一页肯定没有数据了
+      if (data.results.length) {
+        // 如果不为0
+        this.page.page++; // 将页码加1 表示 还有下一页数据
+      } else {
+        this.finished = true; // 表示加载结束
+      }
+    }
   }
+};
 </script>
 
-<style scoped>
-
+<style scoped lang='less'>
+.container {
+  padding-top: 46px;
+  height: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      height: 180px;
+      width: 100%;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    span {
+      padding-right: 10px;
+    }
+  }
+}
 </style>
