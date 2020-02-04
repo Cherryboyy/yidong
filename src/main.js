b1b1245af7017698100e9cac0c43ef59348a3844
cmy <1732728869@qq.com>
2020-02-04 11:57:39 +0800

    完成图片懒加载

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@ import App from './App.vue'
 import router from './permission'
 // import "@/permission"
 import store from './store'
-import Vant from 'vant'
+import Vant, {
+  Lazyload
+} from 'vant'
 import 'vant/lib/index.less'
 import 'amfe-flexible'
 //导入全局样式
 import './styles/index.less'
 //导入时间处理
 import plugin from './utils/plugin'
+//导入图片懒加载插件
 Vue.use(plugin)
+// 注册好了 支持v-lazy指令
+Vue.use(Lazyload)
 
 Vue.use(Vant)
 
