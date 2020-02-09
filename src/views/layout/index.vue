320728c5c545ed94249f5edafc82e24a39336302
cmy <1732728869@qq.com>
2020-02-09 18:33:31 +0800

    完成首页缓存

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="container">
     <!-- 可以采用vant的导航组件 来实现 -->
-    <van-nav-bar @click-right="$router.push('/search')" v-if="showNavBar" fixed title="黑马头条" right-text="搜索" ></van-nav-bar>
+    <van-nav-bar
+      @click-right="$router.push('/search')"
+      v-if="showNavBar"
+      fixed
+      title="黑马头条"
+      right-text="搜索"
+    ></van-nav-bar>
     <!-- 二级路由容器  首页/问答/视频/我的-->
     <!-- :class="{ 名称: 布尔值 }" -->
     <div class="my-wrapper" :class="{ noTop: !showNavBar }">
-      <router-view></router-view>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
     </div>
     <!-- 标签栏  开启路由模式 route :route="true"(布尔)  route="true"(字符串)-->
     <van-tabbar route>
@@ -19,33 +27,33 @@
 </template>
 
 <script>
-  export default {
-    name: 'layout',
-    computed: {
-      // 是否显示navbar
-      showNavBar () {
-        // this  指向 组件实例
-        return this.$route.path !== '/user'
-      }
+export default {
+  name: "layout",
+  computed: {
+    // 是否显示navbar
+    showNavBar() {
+      // this  指向 组件实例
+      return this.$route.path !== "/user";
     }
   }
+};
 </script>
 
 <style scoped lang='less'>
-  .container{
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper {
     width: 100%;
     height: 100%;
-    position: relative;
-    .my-wrapper{
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-      padding-top: 46px;
-      padding-bottom: 50px;
-      box-sizing: border-box;
-      &.noTop{
-        padding-top: 0;
-      }
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop {
+      padding-top: 0;
     }
   }
+}
 </style>
